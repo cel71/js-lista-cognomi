6d72f90290cc98b48488ef9613c3abb27a27b780
cel71 <celestialc71@libero.it>
2021-06-03 17:24:29 +0200

    js-lista-cognomi es1

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+var cognomeUtente = prompt("Scrivi il tuo cognome");
+var cognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+cognomi.push(cognomeUtente);
+cognomi.sort();
+var htmlString = "<ul>";
+for (var i = 0; i < cognomi.length; i++) {
+    htmlString += "<li>" + cognomi[i] + "</li>";
+}
+htmlString += "</ul>";
+document.getElementById("surname").innerHTML = htmlString;
+var listaNumerata = cognomi.indexOf(cognomeUtente);
